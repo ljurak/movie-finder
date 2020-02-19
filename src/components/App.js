cd0fce2f3bc680cce01e668cf564606aed3a5078
ljurak <lucas.jurak@gmail.com>
2020-02-19 23:49:50 +0100

    Add state to main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,69 @@
 import React from 'react';
 
-const App = () => {
-	return (
-		<>
-			<header className="page-header">
-				<h1 className="page-logo">
-					Movie Finder
-				</h1>
-			</header>
-			<div className="container">
-				<aside className="sidebar">
-					<form className="search-form" action="#">
-						<div className="form-row">
-							<input id="title" type="text" />
-							<label htmlFor="title">Title</label>							
-						</div>
-						<div className="form-row">
-							<input id="year" type="text" />
-							<label htmlFor="year">Year</label>							
-						</div>
-						<div className="form-row">
-							<select id="type" value="">
-								<option value="">Type</option>
-							</select>
-							<label htmlFor="type">Type</label>
-						</div>
-						<div className="form-row">
-							<button className="submit-btn">Search</button>
-						</div>
-					</form>
-				</aside>
-				<main className="main-content">
+import SearchForm from './SearchForm';
+import MoviesList from './MoviesList';
 
-				</main>
-			</div>
-		</>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			movies: [],
+			isFetching: false,
+			error: null
+		};
+	}
+
+	componentDidMount() {
+		//this.fetchMovies();
+	}
+
+	fetchMovies = () => {
+		fetch('http://www.omdbapi.com/?apikey=9803e4dd&s=lake')
+			.then(resp => resp.json())
+			.then(movies => this.setState({ movies: movies.Search }));
+	}
+
+	fetchMoviesRequest = () => {
+		this.setState({ isFetching: true, error: null });
+	}
+
+	fetchMoviesSuccess = (movies) => {
+		this.setState({ movies, isFetching: false, error: null });
+	}
+
+	fetchMoviesError = (error) => {
+		this.setState({ isFetching: false, error });
+	}
+
+	render() {
+		const { movies, isFetching, error } = this.state;
+
+		return (
+			<>
+				<header className="page-header">
+					<h1 className="page-logo">
+						Movie Finder
+					</h1>
+				</header>
+				<div className="container">
+					<aside className="sidebar">
+						<SearchForm 
+							fetchMoviesRequest={this.fetchMoviesRequest} 
+							fetchMoviesSuccess={this.fetchMoviesSuccess}
+							fetchMoviesError={this.fetchMoviesError} 
+						/>
+					</aside>
+					<main className="main-content">
+						<MoviesList 
+							movies={movies} 
+							isFetching={isFetching}
+							error={error}
+						/>
+					</main>
+				</div>
+			</>
+		);
+	}
 };
 
 export default App;
