a28fd132f88b4ae41a350a3eee325ade2163fa6e
ljurak <lucas.jurak@gmail.com>
2020-02-20 23:21:46 +0100

    Add api call for next page of movies

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const BASE_API_URL = 'http://www.omdbapi.com/?apikey=9803e4dd';
 
 const MoviesApi = {};
 
-MoviesApi.fetchMovies = function(title, year, type) {
+MoviesApi.searchMovies = function(title, year, type) {
 	let url = BASE_API_URL;
 	if (title) {
 		url += `&s=${title}`;
@@ -16,5 +16,9 @@ MoviesApi.fetchMovies = function(title, year, type) {
 
 	return fetch(url);
 };
+
+MoviesApi.fetchNextPage = function(url, page) {
+	return fetch(`${url}&page=${page}`);
+}
 
 export { MoviesApi };
