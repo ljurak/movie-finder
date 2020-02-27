8cb920388292b3972e7f558cee76d58ebe25274f
ljurak <lucas.jurak@gmail.com>
2020-02-28 00:38:48 +0100

    Add modal for movie details

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const MovieItem = ({ movie }) => (
-	<article className="movie-box">
-		<div className="movie-wrapper">
-			<figure className="movie-photo">
-				<img src={movie.Poster} alt="Movie poster" />
-			</figure>
-			<h3 className="movie-title">{movie.Title}</h3>
-			<p className="movie-release">{movie.Year}</p>
-		</div>
-	</article>
-);
+import Modal from './Modal';
+import MovieModal from './MovieModal';
+
+const MovieItem = ({ movie }) => {
+	const [ showModal, setShowModal ] = useState(false);
+
+	const openModal = () => setShowModal(true);
+
+	const closeModal = (e) => {
+		if (e.target === e.currentTarget) {
+			setShowModal(false);
+		}
+	};
+
+	return (
+		<article className="movie-box">
+			<div className="movie-wrapper" onClick={openModal}>
+				<figure className="movie-photo">
+					<img src={movie.Poster} alt="Movie poster" />
+				</figure>
+				<h3 className="movie-title">{movie.Title}</h3>
+				<p className="movie-release">{movie.Year}</p>
+			</div>
+			{showModal &&
+				<Modal>
+					<MovieModal id={movie.imdbID} closeModal={closeModal} />
+				</Modal>
+			}
+		</article>
+	);
+};
 
 export default MovieItem;
